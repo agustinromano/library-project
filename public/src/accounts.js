20f25414c29d3dcf526aa3a84d7570b46f305610
Agustin <agsutinromano10@yahoo.com>
2022-08-10 18:23:23 -0400

    final commit

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -19,16 +19,10 @@ function getTotalNumberOfBorrows(account, books) {
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  //should return all of the books taken out by an account with the author embedded
-  //Declare a variable that will store the value of the final result in an empty array.
   let possessed = [];
-  //Declare a variable that will store the value of the matching borrow object.
   let list = [];
-  //Loop through the books array using the forEach method and loop through the borrows array using forEach
   books.forEach((item) => {
-    //Declare a variable for just the borrows object const borrowed = item.borrows.
     const borrowed = item.borrows;
-    //Destructure the book object.
     const book = {
       id: item.id,
       title: item.title,
@@ -37,14 +31,11 @@ function getBooksPossessedByAccount(account, books, authors) {
       author: {},
       borrows: {},
     };
-    //As we loop through the borrowed array check if borrow.id is equal to accountId and borrow.returned == false.
     borrowed.forEach((borrow) => {
       if (borrow.id === account.id && borrow.returned === false) {
-        //If conditional is true push the book object into the result array and the borrows object to borrowsMatch array.
         possessed.push(book);
         list.push(borrow);
         book.borrows = list;
-        //Filter through the authors array to return one author object whose id matches.
         book.author = authors.filter((auth) => auth.id === book.authorId)[0];
       }
     });
